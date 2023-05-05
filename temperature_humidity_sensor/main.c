d73a03da285609f7f7c2b215235d64d69c7acfbd
Rigel Pang <rigel_pang@phison.com>
2023-05-05 21:31:38 +0800

    feature(8051): delay 1 ms function
    typedef fixed width integer type and unsigned integer

--- temperature_humidity_sensor/main.c
+++ temperature_humidity_sensor/main.c
@@ -1,34 +1,56 @@
+#include "my_lib.h"
 #include <regx51.h>
 
 #include <stdio.h>
 
-void delay(int count)
+void delay1()
 {
-    int i;
-    for (i = 0; i < count; i++)
+    TMOD = (TMOD & ~T0_MASK_) | (T0_M0_); // TMOD: timer 0 as timer, mode 1 16-bit timer, disable INT
+    TH0 = (0x10000 - CYCLE_PER_MS + 17) / 0x100;
+    TL0 = (0x10000 - CYCLE_PER_MS + 17) % 0x100;
+    TR0 = 1;     // Turn on timer 0
+    while (!TF0) // Wait until timer overflow
         ;
+    TF0 = 0; // Reset timer 0 flag
+    TR0 = 0; // Turn off timer 0
+    return;
+}
+
+void delay(uint16 ms)
+{
+    while (ms--)
+    {
+        delay1();
+    }
 }
 
 void init_uart()
 {
-    TMOD = (TMOD & T0_MASK_) | (T1_M1_); // TMOD: timer 1 as timer, mode 2, 8-bit reload, disable INT
-    SM0 = 0;
-    SM1 = 1;    // SCON: serial port mode 1
-    TH1 = 0xFD; // Set baud rate, reload value =
-    TR1 = 1;    // Trun-on timer 1
-    REN = 1;    // Enable revicer mode
-    ES = 1;     // Enable UART interrupt
-    ET1 = 1;    // Enable timer 1 interrupt
-    TI = 1;     // UART ready
+    TMOD = (TMOD & ~T1_MASK_) | (T1_M1_); // TMOD: timer 1 as timer, mode 2, 8-bit reload, disable INT
+    SM0 = 0;                              // SCON: SM0 = 0, SM1 = 1 -> mode1
+    SM1 = 1;                              // SCON: serial port mode 1
+    TH1 = 0xFD;                           // Set baud rate, TH1 = 0x100 - 2^SMOD * F_osc / (32 * 12 * Bard rate)
+    TR1 = 1;                              // Trun-on timer 1
+    REN = 1;                              // Enable revicer mode
+    ES = 1;                               // Enable UART interrupt
+    ET1 = 1;                              // Enable timer 1 interrupt
+    TI = 1;                               // UART ready
+}
+
+void stop()
+{
+    while (1)
+        ;
 }
 
 void main()
 {
     init_uart();
-    while (1)
+    while (TRUE)
     {
-        P0 = ~P0;
-        printf("hello world\n");
-        delay(10000);
+        delay(1000);
+        printf("test\n");
     }
+    stop();
+    return;
 }
---
+++ temperature_humidity_sensor/my_lib.h
@@ -0,0 +1,17 @@
+#ifndef _MY_LIB_H_
+#define _MY_LIB_H_
+
+#define TRUE 1
+#define FALSE 0
+
+#define XTAL_FEQ 11059200
+#define CYCLE_PER_MS 921 // 1ms / (12 / 11.0592MHz) = 921.6
+
+typedef char int8;
+typedef int int16;
+typedef long int32;
+typedef unsigned char uint8;
+typedef unsigned int uint16;
+typedef unsigned long uint32;
+
+#endif // _MY_LIB_H_
